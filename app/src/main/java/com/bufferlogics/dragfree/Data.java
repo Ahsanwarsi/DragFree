ae1f51462a7ea1df636eaaed0514d62829c81e02
ahsan <muhammad.ahsan@kwanso.com>
2017-05-21 18:25:24 +0500

    Initial commit: Project setup, Emiticon images & Data Class.

---
+++ app/src/main/java/com/bufferlogics/dragfree/Data.java
@@ -0,0 +1,30 @@
+package com.bufferlogics.dragfree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ahsanwarsi.
+ * Copyright (c) 2016 BufferLogics. All rights reserved.
+ */
+public class Data {
+
+    public static List<Integer> getEmoticons(){
+        List<Integer> emos = new ArrayList<>();
+        emos.add(R.drawable.angry_ic);
+        emos.add(R.drawable.cheki_ic);
+        emos.add(R.drawable.cool_ic);
+        emos.add(R.drawable.laugh_ic);
+        emos.add(R.drawable.love_ic);
+        emos.add(R.drawable.sad_ic);
+        emos.add(R.drawable.setisfied_ic);
+        emos.add(R.drawable.emoticon_smile_ic);
+        emos.add(R.drawable.surprised_ic);
+        emos.add(R.drawable.tear_ic);
+        emos.add(R.drawable.unhappy_ic);
+        emos.add(R.drawable.yami_ic);
+
+        return emos;
+    }
+
+}
